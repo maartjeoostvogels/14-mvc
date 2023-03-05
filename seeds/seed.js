673f659bcc49d186dd492873ddc4a269a1c9a2c8
Maartje Oostvogels <moostvogels@hotmail.com>
2023-03-05 19:00:06 +0000

    Set up models, controllers and routes. Provided blog and user data.

---
+++ seeds/seed.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { User, Blog } = require('../models');
+
+const userData = require('./userData.json');
+const BlogData = require('./BlogData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blog of BlogData) {
+    await Blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
